b029052241f8081d411ea95c2a857f2f49377745
Harsh Yadav <yharsh579@gmail.com>
2025-07-08 08:47:19 +0530

    logic

--- recursion/SumOfDigits.java
+++ recursion/SumOfDigits.java
@@ -1,7 +1,18 @@
 package recursion;
+import java.util.Scanner;
+
 
 public class SumOfDigits {
+    static void sum(int n){
+
+
+
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt(n);
+
+
         
     }
     
