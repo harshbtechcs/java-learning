b6f1ffca719e1d597cb0a895d3bccc7556e96eda
Harsh Yadav <yharsh579@gmail.com>
2025-07-08 09:30:56 +0530

    count

---
+++ recursion/CountOfDigit.java
@@ -0,0 +1,22 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class CountOfDigit {
+    static int count(int n){
+        if(n==0){
+            return 0;
+        }
+        return 1 + count(n / 10);
+
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+       int p = count(n);
+       System.out.println(p);
+
+    }
+    
+}
