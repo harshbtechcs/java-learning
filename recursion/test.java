f010140249b1184d3d756b97289ae49b9449dc2e
Harsh Yadav <yharsh579@gmail.com>
2025-07-08 11:26:23 +0530

    demo

--- recursion/test.java
+++ recursion/test.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int n=sc .nextInt();
+        System.out.println("hello github");
+
 
     }
     
