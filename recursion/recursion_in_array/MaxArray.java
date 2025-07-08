bb8e7bbbc783406b69017a7dae198a7335301851
Harsh Yadav <yharsh579@gmail.com>
2025-07-08 13:07:06 +0530

    recursion

---
+++ recursion/recursion_in_array/MaxArray.java
@@ -0,0 +1,13 @@
+package recursion.recursion_in_array;
+
+import java.util.Scanner;
+
+public class MaxArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        
+        
+    }
+    
+}
