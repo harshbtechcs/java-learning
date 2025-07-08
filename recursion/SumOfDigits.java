f9b7741d524be674ed2c7c85e0ec17934601036f
Harsh Yadav <yharsh579@gmail.com>
2025-07-08 09:10:18 +0530

    logic

--- recursion/SumOfDigits.java
+++ recursion/SumOfDigits.java
@@ -3,14 +3,28 @@ import java.util.Scanner;
 
 
 public class SumOfDigits {
-    static void sum(int n){
+    static int sum(int n){
+
+        // base case
+        if(n>=0 && n<=9){
+            return n;
+
+        }
+        // recursive work
+        int smallans= sum(n/10);
+        int ans= sum(n/10)+n%10;
+         
+        return ans;
+
 
 
 
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt(n);
+        int n= sc.nextInt();
+         int p= sum(n);
+         System.out.println(p);
 
 
         
