483543548608a4ea43138581b0a4035c8e8ab6a2
Harsh Yadav <yharsh579@gmail.com>
2025-07-08 11:15:31 +0530

    demo

--- recursion/test.java
+++ recursion/test.java
@@ -1,8 +1,12 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
     }
     
 }
